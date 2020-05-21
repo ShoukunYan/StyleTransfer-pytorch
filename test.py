b186768c905da066a926a1512dbf68f1b5bde5c0
amax <amax@admin.cluster.local>
2020-05-21 15:19:45 +0800

    init

---
+++ test.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+from torchvision import transforms, utils
+from PIL import Image
+
+from dataset import read_picture
+from style_AE import *
+
+
+
+
+if __name__ == "__main__":
+
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+    input_img = read_picture('style_samples/content_pictures/sample.jpg').cuda()
+
+    ae_model = Style_AutoEncoder().to(device)
+
+    print('Weights Loading....')
+    ae_model.load_state_dict(torch.load('models/AutoEncoder/ae_style_1.pth'))
+
+    print("Success!")
+
+    out = ae_model(input_img)
+    
+
+
+        
+    
